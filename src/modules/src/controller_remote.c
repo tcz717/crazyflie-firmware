b2ffe108673d392289b31066edd2a5d5d0486f60
Tangent <tcz717@hotmail.com>
2018-09-28 12:55:50 -0400

    Adjust K matrix

--- src/modules/src/controller_remote.c
+++ src/modules/src/controller_remote.c
@@ -35,8 +35,10 @@ bool controllerRemoteTest(void)
 static const double u0[4] = {16008.1328616788, 16008.1328616788, 16008.1328616788, 16008.1328616788};
 static const double K[48] = {
     -465.5, 465.5, 465.5, -465.5, 465.5, 465.5, -465.5, -465.5,
-    15355, 15355, 15355, 15355, -987, -987, 987, 987,
-    -987, 987, 987, -987, -14802, 14802, -14802, 14802,
+    // 15355, 15355, 15355, 15355, -987, -987, 987, 987,
+    15355, 15355, 15355, 15355, 987, -987, -987, 987,
+    -987, -987, 987, 987, -14802, 14802, -14802, 14802,
+    // -987, 987, 987, -987, -14802, 14802, -14802, 14802,
     -342, 342, 342, -342, 342, 342, -342, -342,
     5396, 5396, 5396, 5396, -156, -156, 156, 156,
     -156, 156, 156, -156, -248.9, 248.9, -248.9, 248.9};
@@ -48,14 +50,27 @@ void controllerRemote(control_t *control, setpoint_t *setpoint,
     double pwm[4];
     LQR_controller2(u0, K, 0, 0, 0.3, 0,
                     state->position.x, state->position.y, state->position.z,
-                    state->attitude.pitch, state->attitude.roll, state->attitude.yaw,
-                    sensors->gyro.x, sensors->gyro.y, sensors->gyro.z,
+                    0, 0, 0,
+                    // state->attitude.pitch, state->attitude.roll, state->attitude.yaw,
+                    0, 0, sensors->gyro.z,
+                    // sensors->gyro.x, sensors->gyro.y, sensors->gyro.z,
                     last.x, last.y, last.z, pwm,
                     &last.x, &last.y, &last.z);
 
-    motorSetEnable = true;
-    motorPowerSet.m1 = pwm[0];
-    motorPowerSet.m2 = pwm[1];
-    motorPowerSet.m3 = pwm[2];
-    motorPowerSet.m4 = pwm[3];
+    // if (setpoint->thrust != 0)
+    // {
+        motorSetEnable = true;
+        motorPowerSet.m1 = pwm[0];
+        // motorPowerSet.m2 = pwm[1];
+        motorPowerSet.m3 = pwm[2];
+        // motorPowerSet.m4 = pwm[3];
+    // }
+    // else
+    // {
+    //     motorSetEnable = false;
+    //     motorPowerSet.m1 = 0;
+    //     motorPowerSet.m2 = 0;
+    //     motorPowerSet.m3 = 0;
+    //     motorPowerSet.m4 = 0;
+    // }
 }
